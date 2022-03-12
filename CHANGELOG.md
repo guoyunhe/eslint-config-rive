27faece789cc4a5a5601097d2097b24a4d177c33
Guo Yunhe <i@guoyunhe.me>
2022-03-12 22:24:39 +0800

    feat: typescript + react

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1.0.0
+
+- Extend ESLint's recommended configuration
+- Add React plugin and extend recommended configuration
+- Add TypeScript plugin and parser, and extend recommended configuration
+- Add Prettier plugin and extend recommended configuration
---
+++ index.js
@@ -0,0 +1,22 @@
+const config = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: ['./tsconfig.json'] },
+  plugins: ['@typescript-eslint', 'import', 'react', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
+
+export default config;
