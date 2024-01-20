cb05f6a78c9572194d9d67c7adcff5d412b9a97e
Guo Yunhe <i@guoyunhe.me>
2024-01-20 11:09:10 +0800

    import

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
     'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
@@ -12,17 +11,6 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.d.ts', '.tsx'],
-    },
-    // YOU MUST put typescript before node (see https://github.com/import-js/eslint-plugin-import/issues/2225#issuecomment-923516582)
-    'import/resolver': {
-      typescript: {},
-      node: {},
-    },
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
   },
   ignorePatterns: ['.rive', 'build', 'coverage', 'dist', 'lib', 'node_modules'],
 };
